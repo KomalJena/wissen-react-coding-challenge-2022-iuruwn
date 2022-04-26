3f0b8606db4bae3f485ddd3c5f41e40cced07255
KomalJena <jenakomal3@gmail.com>
2022-04-26 11:58:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wissen-react-coding-challenge-2022-iuruwn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/wissen-react-coding-challenge-2022-iuruwn)
---
+++ index.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import { render } from 'react-dom';
+import { useHistory } from 'react-router-dom';
+import './style.css';
+
+const App = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // const history = useHistory();
+  useEffect(() => {
+    if (localStorage.getItem('user-info')) {
+    }
+  });
+  async function login() {
+    console.warn(email, password);
+    let item = { email, password };
+    let result = await fetch('https://reqres.in/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(item),
+    });
+    result = await result.json();
+    localStorage.setItem('user-info', JSON.stringify(result));
+  }
+  return (
+    <div className="main">
+      <div>
+        <div>
+          <h1>Hello there, Sign in to continue</h1>
+
+          <div>
+            <form className="form">
+              <div>
+                <label className="label">Email</label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <div></div>
+              </div>
+              <div>
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <div></div>
+              </div>
+              <button className="btn" onClick={login}>
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
